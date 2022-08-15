f8fe29cd2a5e0c4acc1a5b84dadcc0730382a1e5
kabeer-prog <kabeerlap@gmail.com>
2022-08-15 12:57:50 +0100

    Added package

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-35jog8iq.us.auth0.com',
+  clientId: 'kkafbUKzuOykdpVOHtkrl1QX5douObek',
+  callbackUrl: 'http://localhost:3000/callback'
+};
